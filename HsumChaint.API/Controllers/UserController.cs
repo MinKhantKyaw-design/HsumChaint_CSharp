8b638287b241e6789e7ed62350f0b2432423d79d
Nyan Oak Soe <nyanoaksoe77@gmail.com>
2026-03-05 09:41:08 +0630

    fix: controller name

--- HsumChaint.API/Controllers/UserController.cs
+++ HsumChaint.API/Controllers/UserController.cs
@@ -7,11 +7,11 @@ namespace HsumChaint.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
 
-        public ValuesController(IUserService userService)
+        public UserController(IUserService userService)
         {
             _userService = userService;
         }
